dc4446012997bfda0587f4be7a3f9a1310524c7a
David Ferri <d.ferri.2018@alumnos.urjc.es>
2019-03-17 00:25:50 +0100

    chat_DavidFerri_CarlosQuintana

---
+++ chat_cliente.py
@@ -0,0 +1,20 @@
+import socket
+
+puerto = 8080
+ip = "10.0.2.15"
+def chat(cliente):
+    condicion = True
+    while condicion:
+        mensaje_cliente = input("")
+        mensaje_cliente = str.encode(mensaje_cliente)
+        cliente.send(mensaje_cliente)
+        mensaje_servidor = cliente.recv(2048).decode("utf-8")
+        print(mensaje_servidor)
+        
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    cliente.connect((ip, puerto))
+    chat(cliente)
+except KeyboardInterrupt:
+    cliente.close()
+print("La conversación se acabó.")
---
+++ chat_servidor.py
@@ -0,0 +1,26 @@
+import socket
+ip = "10.0.2.15"
+puerto = 8080
+respuestas = 2
+def lee_mensaje(cliente):
+    condicion = True
+    while condicion:
+        mensaje_cliente = cliente.recv(2048).decode("utf-8")
+        print(mensaje_cliente)
+        mensaje_servidor = input("")
+        mensaje_servidor = str.encode(mensaje_servidor)
+        cliente.send(mensaje_servidor)
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    serversocket.bind((ip, puerto))
+    serversocket.listen(respuestas)
+    print("Esperando conexion en el puerto:",puerto,"y en la ip:",ip)
+    (cliente, address) = serversocket.accept()
+    lee_mensaje(cliente)
+
+except KeyboardInterrupt:
+    cliente.close()
+    serversocket.close()
+print("La conversación se acabó...")
